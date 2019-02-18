a8c81ca5db71896433225db23d1f238733f18730
Jessica Brown <jessica-e-brown@uiowa.edu>
2019-02-18 13:56:49 -0600

    upgrade to silverstripe 4
    missing uiowa search bar

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,24 +1,16 @@
 <?php
 
-global $project;
-$project = 'mysite';
+use SilverStripe\Security\PasswordValidator;
+use SilverStripe\Security\Member;
+use SilverStripe\Control\Director;
 
-global $database;
-$database = 'powwow';
- 
-// Use _ss_environment.php file for configuration
-require_once("conf/ConfigureFromEnv.php");
-MySQLDatabase::set_connection_charset('utf8');
+// remove PasswordValidator for SilverStripe 5.0
+$validator = new PasswordValidator();
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
-// Set the site locale
-i18n::set_locale('en_US');
-FulltextSearchable::enable();
+$validator->minLength(8);
+$validator->checkHistoricalPasswords(6);
+Member::set_password_validator($validator);
 
 if(Director::isLive()) {
-	Director::forceSSL(array('/^Security/','/^admin/'));
+	Director::forceSSL();
 }
-Authenticator::set_default_authenticator('SAMLAuthenticator');
--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,4 +1,9 @@
 <?php
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+
 class Page extends SiteTree {
 
 	private static $db = array(
@@ -17,36 +22,5 @@ class Page extends SiteTree {
 		$fields->removeFieldFromTab("Root.Main","Content");
 
 		return $fields;
-	}
-
-
-}
-class Page_Controller extends ContentController {
-
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array (
-	);
-
-	public function init() {
-		parent::init();
-
-		// Note: you should use SS template require tags inside your templates 
-		// instead of putting Requirements calls here.  However these are 
-		// included so that our older themes still work
-
 	}
 }
